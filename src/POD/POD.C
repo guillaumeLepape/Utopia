e5159bab5f9b41670cb02c000b0c0963f21a21c5
Guillaume Le Pape <gui.lepape25@gmail.com>
2020-08-08 01:52:25 +0200

    Add structure of POD class

--- src/POD/POD.C
+++ src/POD/POD.C
@@ -2,17 +2,22 @@
 
 namespace Utopia {
 
-  namespace POD {
-
-    template<class T>
-    Topos_Chronos<T> performPOD()
-    {
-      Topos_Chronos<T> topos_chronos;
-
-      return topos_chronos;
-    }
-
+  template<class T>
+  POD<T>::POD(const Time& runTime, const fvMesh& mesh, const IOdictionary& UtopiaDict) :
+    runTime_(runTime),
+    mesh_(mesh),
+    UtopiaDict_(UtopiaDict),
+    nModes_(UtopiaDict.lookupOrDefault<label>("nModes",1)),
+    topos_(nModes_),
+    chronos_(nModes_)
+  {
+    compute();
   }
 
+  template<class T>
+  void POD<T>::compute()
+  {
+    
+  }
 
 }
